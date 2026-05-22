f73bdc247ef0dcd5d295efd1b664a854608d6b67
Hirotoshi Uchida <uho02741358@gmail.com>
2026-05-22 22:44:07 +0900

    Update main.py

--- services/python/fastapi/main.py
+++ services/python/fastapi/main.py
@@ -10,6 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://omnitex.vercel.app",
+        "https://omnitex-ph5mbqfhh-uchida16104s-projects.vercel.app",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
     ],
